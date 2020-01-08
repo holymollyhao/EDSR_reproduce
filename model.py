e181bed7a1429ef6a5c50e4427877e1ba08a707f
Kim Tae Won <maxkim139@gmail.com>
2020-01-08 16:11:57 +0900

    Update model.py

--- model.py
+++ model.py
@@ -10,34 +10,69 @@ import matplotlib.pyplot as plt
 #resdiual 기본 블록
 
 def res_block(input_tensor, kernel_size, filters):
-    """The identity block is the block that has no conv layer at shortcut.
-    # Arguments
-        input_tensor: input tensor
-        kernel_size: default 3, the kernel size of
-            middle conv layer at main path
-        filters: list of integers, the filters of 3 conv layer at main path
-    """
-    filters1, filters2 = filters
+  
+  # filters: tuple, 순서대로 필터 num
+  # kernel_size: tuple, kernel size 입력
+  # input_tensor: 텐서가 들어간다 
 
-    #first convolution filter, kernel_initializer는 optional
-    x = keras.layers.Conv2D(filters1, kernel_size = (3, 3),
-    				  padding ='same',
-                      #kernel_initializer='he_normal',
-                      #kernel_regularizer=regularizers.l2(L2_WEIGHT_DECAY)
-                      )(input_tensor)
-    #relu
-    x = keras.layers.Activation('relu')(x)
 
-    #second convolution filter
-    x = keras.layers.Conv2D(filters2, kernel_size = (3, 3),
-                      padding='same',
-                      #kernel_initializer='he_normal',
-                      #kernel_regularizer=regularizers.l2(L2_WEIGHT_DECAY)
-                      )(x)
+  filters1, filters2 = filters
 
-    #residual scaling 
-    x = x * 0.1 
+  #first convolution filter, kernel_initializer는 optional
+  x = keras.layers.Conv2D(filters1, kernel_size,
+  				          padding ='same',
+                    #kernel_initializer='he_normal',
+                    #kernel_regularizer=regularizers.l2(L2_WEIGHT_DECAY)
+                    )(input_tensor)
+  #relu
+  x = keras.layers.Activation('relu')(x)
 
-	x = keras.layers.Add()[x, input_tensor]
+  #second convolution filter
+  x = keras.layers.Conv2D(filters2, kernel_size,
+                    padding='same',
+                    #kernel_initializer='he_normal',
+                    #kernel_regularizer=regularizers.l2(L2_WEIGHT_DECAY)
+                    )(x)
 
-    return x
+  #residual scaling 
+  x = x * 0.1 
+
+  x = keras.layers.Add()[x, input_tensor]
+
+  return x
+
+def edsr(input_tensor, scale, num_layers):
+
+  kernel_size = (3, 3)
+
+  #frist convolution
+  x = keras.layers.Conv2D(256, kernel_size,
+                    padding ='same',
+                    #kernel_initializer='he_normal',
+                    #kernel_regularizer=regularizers.l2(L2_WEIGHT_DECAY)
+                    )(input_tensor)
+
+  #save data for future addition
+  x_pre = x 
+
+  #resblocks
+  for i in range(num_layers):
+    x = res_block(x, kernel_size, (256, 256))
+  
+  x = keras.layers.Conv2D(256, kernel_size,
+                    padding ='same',
+                    #kernel_initializer='he_normal',
+                    #kernel_regularizer=regularizers.l2(L2_WEIGHT_DECAY)
+                    )(x)
+  x = x + x_pre
+
+
+
+
+
+
+
+
+
+
+
