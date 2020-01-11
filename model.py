c0f6b6e70a146083a1a95e3f8f2268883387c062
Kim Tae Won <maxkim139@gmail.com>
2020-01-11 15:42:49 +0900

    Update model.py

--- model.py
+++ model.py
@@ -31,11 +31,11 @@ def res_block(input, num_filters, resblock_scaling):
 def upscale_block(input, num_filters, scale):
   if scale == 2:
     x = keras.layers.Conv2D(num_filters * (scale ** 2), 3, padding = 'same')(input)
-    x = (tf.nn.depth_to_space(x, scale))x
+    x = (tf.nn.depth_to_space(x, scale))(x)
 
   elif scale == 3:
     x = keras.layers.Conv2D(num_filters * (scale ** 2), 3, padding = 'same')(input)
-    x = tf.nn.depth_to_space(x, scale)
+    x = tf.nn.depth_to_space(x, scale)(x)
 
   elif scale ==4:
     x = keras.layers.Conv2D(num_filters * (scale ** 2), 3, padding = 'same')(input)
