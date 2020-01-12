44d87516303e49ac514dfe7d688638c83e1164a2
Kim Tae Won <maxkim139@gmail.com>
2020-01-12 16:01:47 +0900

    Update model2.py

--- model2.py
+++ model2.py
@@ -72,7 +72,7 @@ train_ds = train.dataset(batch_size=16, random_transform=True)
 os.makedirs("~/weights", exist_ok = True)
 
 edsr_model = edsr(scale=4, num_resblocks=16)
-edsr_model.load_model('edsr_model.h5')
+edsr_model.load_weights(os.path.join("~/weights", 'weights-edsr-16-x4.h5'))
 edsr_model.summary()
 test_img = plt.imread("./dataset/images240/frame0.jpg")
 
