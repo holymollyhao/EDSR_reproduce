f8ffdbe46fd069977a4329d493ad3d8f8de33ab5
Kim Tae Won <maxkim139@gmail.com>
2020-01-12 16:26:43 +0900

    Update model2.py

--- model2.py
+++ model2.py
@@ -74,7 +74,7 @@ os.makedirs("~/weights", exist_ok = True)
 edsr_model = edsr(scale=4, num_resblocks=16)
 edsr_model.load_weights(os.path.join("~/weights", 'weights-edsr-16-x4.h5'))
 edsr_model.summary()
-test_img = plt.imread("./dataset/images240/frame0.jpg")
+test_img = tf.io.read_file("./dataset/images240/frame0.jpg")
 
 
 
