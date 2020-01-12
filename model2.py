cc1a95b1a11d3660af68cc8e2cbc64ff34bfbac7
Kim Tae Won <maxkim139@gmail.com>
2020-01-12 15:56:01 +0900

    Update model2.py

--- model2.py
+++ model2.py
@@ -73,11 +73,11 @@ def edsr(scale = 2, num_filters = 64, num_resblocks = 16, resblock_scaling = Non
 train = DIV2K(scale=4, downgrade='bicubic', subset='train')
 train_ds = train.dataset(batch_size=16, random_transform=True)
 os.makedirs("~/weights", exist_ok = True)
-'''
+
 edsr_model = edsr(scale=4, num_resblocks=16)
 edsr_model.load_model('edsr_model.h5')
 edsr_model.summary()
 test_img = plt.imread("./dataset/images240/frame0.jpg")
-
+'''
 
 
