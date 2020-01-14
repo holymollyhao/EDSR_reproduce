7405ca67442551e8935e3f8803574e33e8b702bc
Kim Tae Won <maxkim139@gmail.com>
2020-01-14 22:03:53 +0900

    Update extract_frame.py

--- extract_frame.py
+++ extract_frame.py
@@ -75,15 +75,4 @@ def imagetovid(imgpath, vidpath, vidname, fps):
     
     out.release()
 
-'''
-frameRate = cap.get(1) #frame rate
-while(cap.isOpened()):
-    frameId = cap.get(1) #current frame number
-    ret, frame = cap.read()
-    if (ret != True):
-        break
-    if (frameId % math.floor(frameRate) == 0):
-        filename = imagesFolder + "/image_" +  str(int(frameId)) + ".jpg"
-        cv2.imwrite(filename, frame)
-cap.release()
-'''
+    
